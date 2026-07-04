1fa670eac1e70c5ebe765eab273456a21259aadc
kavyanayak0000-blip <Kavyanayak0000@gmail.com>
2026-07-04 18:16:58 +0530

    new changes

--- src/test/java/SeleniumJavaAssignment/BaseClass.java
+++ src/test/java/SeleniumJavaAssignment/BaseClass.java
@@ -8,98 +8,120 @@ import java.util.HashMap;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
-import org.testng.annotations.Parameters;
 
 public class BaseClass {
 
-	private String username = "";
-	private String accesskey = "";
-	private String hubURL = "hub.lambdatest.com/wd/hub";
+    private String username = "";
+    private String accesskey = "";
+    private String hubURL = "hub.lambdatest.com/wd/hub";
 
-	protected RemoteWebDriver driver;
+    // Thread-safe WebDriver
+    protected static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<>();
 
+    // Getter
+    public static RemoteWebDriver getDriver() {
+        return driver.get();
+    }
 
+    @SuppressWarnings("deprecation")
 	@Parameters({"platformname","browser","browserversion"})
-	@BeforeTest
-	public void setUp(String platformname,String browser,String browserversion) {
+    @BeforeTest
+    public void setUp(String platformname,
+                      String browser,
+                      String browserversion) {
 
-		DesiredCapabilities capabilities = new DesiredCapabilities();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
 
-		capabilities.setCapability("platformName", platformname);
-		capabilities.setCapability("browserName", browser);
-		capabilities.setCapability("browserVersion", browserversion); 
+        capabilities.setCapability("platformName", platformname);
+        capabilities.setCapability("browserName", browser);
+        capabilities.setCapability("browserVersion", browserversion);
 
+        HashMap<String, Object> ltOptions = new HashMap<>();
 
-		// LambdaTest-specific options
-		HashMap<String, Object> ltOptions = new HashMap<>();
+        ltOptions.put("username", username);
+        ltOptions.put("accessKey", accesskey);
+        ltOptions.put("visual", true);
+        ltOptions.put("video", true);
+        ltOptions.put("network", true);
+        ltOptions.put("build", "Final Build");
+        ltOptions.put("project", "LambdaTest");
+        ltOptions.put("name", browser + " Execution");
+        ltOptions.put("console", true);
+        ltOptions.put("terminal", true);
+        ltOptions.put("w3c", true);
 
-		ltOptions.put("username", username); //user name
-		ltOptions.put("accessKey", accesskey); //Password
-		ltOptions.put("visual", true); //Screenshots
-		ltOptions.put("video", true); //Video Recording
-		ltOptions.put("network", true); //Network Log
-		ltOptions.put("build", "final-Build"); //Build Name
-		ltOptions.put("project", "LabmdaTest"); //Project Name
-		ltOptions.put("name", "Selenium Java Lambda Test"); //Test Name 
-		ltOptions.put("tunnel", false); // Disabling the tunnel for anywhere accessible Uri
-		ltOptions.put("console", true); //Console Log
-		ltOptions.put("w3c", true); // Enable W3C WebDriver protocol for compatibility with the latest WebDriver standards
-		ltOptions.put("terminal", true); //terminal Log
+        capabilities.setCapability("LT:Options", ltOptions);
 
+        try {
 
-		// Set the LT options in ChromeOptions
-		capabilities.setCapability("LT:Options", ltOptions);
+            driver.set(new RemoteWebDriver(
+                    new URL("https://" + username + ":" + accesskey + "@" + hubURL),
+                    capabilities));
 
-		// Create RemoteWebDriver instance and connect to LambdaTest cloud
-		try {
-			driver = new RemoteWebDriver(new URL("https://" + username + ":" + accesskey + "@" + hubURL), capabilities);
-		} catch (MalformedURLException e) {
-			System.out.println("Invalid grid URL: " + e.getMessage());
-			e.printStackTrace();
-		} catch (Exception e) {
-			System.out.println("Error while initializing driver: " + e.getMessage());
-			e.printStackTrace();
-		}
+        } catch (MalformedURLException e) {
 
-	}
-	//@BeforeTest
-	public void startUp() {
-		driver = new ChromeDriver();
-	}
-	@BeforeMethod
-	public void setURI() {
-		if (driver != null) {
-			driver.manage().window().maximize();
-			driver.get("https://www.lambdatest.com/selenium-playground/");
-		} else {
-			System.out.println("Driver is not initialized!");
-		}
-	}
-	@AfterMethod
-	public void onFailureTakeSS(ITestResult testResult) {
+            e.printStackTrace();
 
-		TakesScreenshot snap = (TakesScreenshot) driver;
-		File source = snap.getScreenshotAs(OutputType.FILE);
-		File Destination = new File(System.getProperty("user.dir")+ "/snaps/"+ testResult.getName() + ".png");
+        }
 
-		try {
-			FileHandler.copy(source, Destination);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    }
 
-	@AfterTest
-	public void tearDown() {
-		if (driver != null) {
-			driver.quit();
-		}
-	}
+    @BeforeMethod
+    public void setURI() {
+
+        getDriver().manage().window().maximize();
+
+        getDriver().get("https://www.testmuai.com/selenium-playground/");
+
+    }
+
+    @AfterMethod
+    public void takeScreenshot(ITestResult result) {
+
+        if (!result.isSuccess()) {
+
+            TakesScreenshot ts = (TakesScreenshot) getDriver();
+
+            File source = ts.getScreenshotAs(OutputType.FILE);
+
+            File destination = new File(
+                    System.getProperty("user.dir")
+                            + "/snaps/"
+                            + result.getName()
+                            + "_"
+                            + Thread.currentThread().getId()
+                            + ".png");
+
+            try {
+
+                FileHandler.copy(source, destination);
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+
+        }
+
+    }
+
+    @AfterTest
+    public void tearDown() {
+
+        if (getDriver() != null) {
+
+            getDriver().quit();
+
+            driver.remove();
+
+        }
+
+    }
+
 }
